001199e72c48d1335bdaf2324f17a3339255b43a
malaikajaved <malaikajaved562@gmail.com>
2026-06-24 23:38:43 +0500

    Updated website

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.PNG', '**/*.png'], 
+  assetsInclude: ['**/*.PNG', '**/*.png'],
+  publicDir: 'public', // ensures _redirects is copied to dist
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 });
